48e2bf0ed213cd22125f3c02aaf886d2e1d96d0e
Jake G <jake098877@gmail.com>
2020-05-04 15:33:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,27 @@ class School
 
   def roster
     @roster
+<<<<<<< HEAD
+  end
+
+
+
+  def school
+    @school
+  end
+
+  def add_student (name, grade)
+    if @roster.include?(grade)
+          @roster[grade] << "#{name}"
+    else
+      @roster[grade] = []
+      @roster[grade] << "#{name}"
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+=======
   end
 
 
@@ -30,6 +51,25 @@ class School
   end
 
 def sort
+  @roster.each do |grade, name|
+    @roster[grade] = name
+    name.sort! { |alpha, beta| alpha <=> beta }
+
+>>>>>>> cd921a2c1f03448084b1d39c827f431fe1536bb6
+  end
+    @roster =  @roster.sort
+
+        # if a == b
+        #   0
+        # elsif a < b
+        #   -1
+        # elsif a > b
+        #   1
+        # end
+
+
+<<<<<<< HEAD
+def sort
   hash = {}
   @roster.each do |grade, name|
       hash[grade] = []
@@ -38,5 +78,7 @@ def sort
 
   end
     # @roster = hash.sort
+=======
+>>>>>>> cd921a2c1f03448084b1d39c827f431fe1536bb6
     end
 end
